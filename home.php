37c70f6262bb4c7fc154def89039776e64d76d19
ridia178 <ridia.kashmeri1011@gmail.com>
2023-10-15 13:05:15 +0800

    home changed

--- home.php
+++ home.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION["valid"])) {
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="style/style.css" />
-  <title>Home</title>
+  <title> welcome Home</title>
 </head>
 
 <body>
