2f85a4737e20b821cf9fe3f123351db7b437e877
ridia178 <ridia.kashmeri1011@gmail.com>
2023-11-14 17:16:42 +0800

    Contact Page construction

--- contact.php
+++ contact.php
@@ -10,27 +10,30 @@
 </head>
 
 <body>
-
+  <!---------------------------------------------------------------ABOUT US------------------------------------------------------------->
   <div class="about">
     <div class="about-box">
       <img class="logo" src="images/logo.png" alt="">
       <div class="about-content">
         <h3>About Us</h3>
         <p id="calventerContent"> Discover "Calventer", your ultimate event calendar for IIUM's social clubs!
-          We bring you a seamless platform to stay informed, manage your time, and never miss out on the exciting 
-          happenings at your university."Calventer" is the go-to web application designed exclusively for IIUM's 
-          vibrant social club scene. Our mission is to eliminate the chaos caused by conflicting events, allowing 
-          club owners to effortlessly schedule and update their activities without any clashes. With "Calventer", 
+          We bring you a seamless platform to stay informed, manage your time, and never miss out on the exciting
+          happenings at your university."Calventer" is the go-to web application designed exclusively for IIUM's
+          vibrant social clubs. Our mission is to eliminate the chaos caused by conflicting events, allowing
+          club owners to effortlessly schedule and update their activities without any clashes. Through "Calventer",
           you can access event details, plan your schedule, and ensure a smooth flow of activities throughout the
-          academic year. But that's not all - "Calventer" is a community platform! Students and members can easily 
-          explore the calendar, keeping themselves informed about upcoming events. Whether it's a stimulating workshop, 
-          a thrilling sports tournament, or an engaging club activity, "Calventer" provides all the essential information 
-          to help you manage your time effectively and attend the events that interest you the most.We believe in the power 
-          of collaboration and user empowerment. That's why "Calventer" allows you to actively engage with the platform. 
-          Club owners can easily add and modify their event information, ensuring the calendar remains up to date. 
-          Students and members have the freedom to browse, discover, and make the most of their university experience.
+          academic year. But that's not all - "Calventer" is a community platform! Students and members can easily
+          explore the calendar, keeping themselves informed about upcoming events. Whether it's a stimulating workshop,
+          a thrilling sports tournament, or an engaging club activity, "Calventer" provides all the essential
+          information
+          to help you manage your time effectively and attend the events that interest you the most.We believe in the
+          power
+          of collaboration and user empowerment. That's why "Calventer" allows you to actively engage with the platform.
+          Club owners can easily add and modify their event informations, ensuring the calendar remains up to date.
+          Students and members have the freedom to browse, discover, and make the most of their university experiences.
           Join "Calventer" today and unlock a world of possibilities, where managing your time and attending events
-          becomes a breeze. Embrace the vibrant spirit of IIUM's social clubs and create unforgettable memories along the way.
+          becomes a breeze. Embrace the vibrant spirit of IIUM's social clubs and create unforgettable memories along
+          the way.
           Together, let's make the most of your university journey!
         </p>
         <a href="#" id="readMoreLink">Read More</a>
@@ -45,7 +48,7 @@
               if (content.style.maxHeight) {
                 content.style.maxHeight = null;
               } else {
-                content.style.maxHeight = content.scrollHeight + 'px'; 
+                content.style.maxHeight = content.scrollHeight + 'px';
               }
 
               link.textContent = link.textContent === 'Read More' ? 'Read Less' : 'Read More';
@@ -56,7 +59,82 @@
     </div>
   </div>
 
-  <div class="content">
+
+  <!---------------------------------------------------------------TEAM------------------------------------------------------------->
+
+  <div class="slide-container">
+    <div class="slide-content">
+      <div class="card-wrapper">
+        <div class="card">
+          <div class="image-content">
+            <span class="overlay"></span>
+
+            <div class="card-image">
+              <img src="images/madam.png" alt="" class="card-img">
+            </div>
+          </div>
+
+          <div class="card-content">
+            <h2 class="name">Marini Binti Othman (Dr.)</h2>
+            <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Eos animi, molestias tenetur iusto aliquid harum porro quis totam, exercitationem
+              laudantium ea quia similique, eaque aliquam eius. Consectetur exercitationem accusamus</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- <div class="content">
     <section id="team">
       <div class="team-heading">
         <h3>team</h3>
@@ -119,7 +197,7 @@
         },
       });
     </script>
-  </div>
+  </div> -->
 
   <div class="container">
     <div class="box form-box">
