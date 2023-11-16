c80b7c7de3f34bf49cdd61a5d2131773a92cda3d
ridia178 <ridia.kashmeri1011@gmail.com>
2023-11-16 15:50:30 +0800

    prep for cover page

---
+++ cover.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style/cover.css" />
+  <title>Cover</title>
+</head>
+<body>
+<div class="video-container">
+    <video autoplay loop muted>
+        <source src="images/cover.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+</div>  
+</body>
+</html>
