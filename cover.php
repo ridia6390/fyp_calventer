afa92a3f7043ff9feff9dbc9310a304da6963700
ridia178 <ridia.kashmeri1011@gmail.com>
2023-11-16 19:03:28 +0800

    cover page modification

--- cover.php
+++ cover.php
@@ -1,17 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="style/cover.css" />
   <title>Cover</title>
 </head>
+
 <body>
-<div class="video-container">
+  <div class="video-container">
     <video autoplay loop muted>
-        <source src="images/cover.mp4" type="video/mp4">
-        Your browser does not support the video tag.
+      <source src="images/cover.mp4" type="video/mp4">
     </video>
-</div>  
+    <div class="color-overlay">
+    <img class="iiumlogo" src="images/iiumlogo.png" alt="">
+    <img class="iiumtagline" src="images/iiumtagline.png" alt=""> 
+    <img class="logo" src="images/logo.png" alt="">
+    <div class="video-border">
+      <h3>gediwegi</h3>
+    </div>
+
+    </div>
+  </div>
 </body>
+
 </html>
