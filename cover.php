e6bab6109066d41364a910b906b647764b6f66a4
ridia178 <ridia.kashmeri1011@gmail.com>
2023-11-17 20:37:12 +0800

    landing page button

--- cover.php
+++ cover.php
@@ -14,12 +14,20 @@
       <source src="images/cover.mp4" type="video/mp4">
     </video>
     <div class="color-overlay">
-    <img class="iiumlogo" src="images/iiumlogo.png" alt="">
-    <img class="iiumtagline" src="images/iiumtagline.png" alt=""> 
-    <img class="logo" src="images/logo.png" alt="">
-    <div class="video-border">
-      <h3>gediwegi</h3>
-    </div>
+      <img class="iiumlogo" src="images/iiumlogo.png" alt="">
+      <img class="iiumtagline" src="images/iiumtagline.png" alt="">
+      <img class="logo" src="images/logo.png" alt="">
+      <h3 class="project-name">Calventer</h3>
+      <h4 class="project-tagline">Event Calender For IIUM's <br> Social Clubs</h4>
+      <div class="video-border">
+        <h3>gediwegi</h3>
+      </div>
+      <div class="club-admin">
+        <input type="submit" class="btn" name="submit" value="Club Admins">
+      </div>
+      <div class="event-viewer">
+      <input type="submit" class="btn" name="submit" value="Event Viewers">
+      </div>
 
     </div>
   </div>
