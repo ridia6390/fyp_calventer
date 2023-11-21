82085260ea7d4cb35765d0da6c24821c0d2ba1f9
ridia178 <ridia.kashmeri1011@gmail.com>
2023-11-21 13:20:40 +0800

    cover page finalisation

--- cover.php
+++ cover.php
@@ -15,20 +15,18 @@
     </video>
     <div class="color-overlay">
       <img class="iiumlogo" src="images/iiumlogo.png" alt="">
-      <img class="iiumtagline" src="images/iiumtagline.png" alt="">
       <img class="logo" src="images/logo.png" alt="">
       <h3 class="project-name">Calventer</h3>
       <h4 class="project-tagline">Event Calender For IIUM's <br> Social Clubs</h4>
-      <div class="video-border">
-        <h3>gediwegi</h3>
-      </div>
       <div class="club-admin">
-        <input type="submit" class="btn" name="submit" value="Club Admins">
+        <input type="submit" class="btn" name="submit" value="Club Admins" >
       </div>
       <div class="event-viewer">
-      <input type="submit" class="btn" name="submit" value="Event Viewers">
+      <input type="submit" class="btn" name="submit" value="Event Viewers" >
       </div>
-
+      <div class="video-border">
+        <h3>Calventer: Your Navigator through the Maze of IIUM's Social Club Events!</h3>
+      </div>
     </div>
   </div>
 </body>
