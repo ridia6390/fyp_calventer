b3fe7409b364d81296539644e2c4badc2afc0172
ridia178 <ridia.kashmeri1011@gmail.com>
2023-12-26 06:04:50 +0800

    done adding user profile

--- adminHome.php
+++ adminHome.php
@@ -2,11 +2,12 @@
 session_start();
 
 // Function to get user information based on ID
-function getUserInfo($id) {
-   global $conn;
-   $select = $conn->prepare("SELECT * FROM `admins` WHERE id = ?");
-   $select->execute([$id]);
-   return $select->fetch(PDO::FETCH_ASSOC);
+function getUserInfo($id)
+{
+  global $conn;
+  $select = $conn->prepare("SELECT * FROM `admins` WHERE id = ?");
+  $select->execute([$id]);
+  return $select->fetch(PDO::FETCH_ASSOC);
 }
 
 ?>
@@ -16,9 +17,9 @@ include 'admins.php';
 
 // Check if the user is logged in
 if (!isset($_SESSION['admin_id'])) {
-    // Redirect to the login page if not logged in
-    header('location: login.php');
-    exit();
+  // Redirect to the login page if not logged in
+  header('location: login.php');
+  exit();
 }
 
 // Retrieve user information from the session
@@ -29,9 +30,9 @@ $user_info = getUserInfo($admin_id);
 
 // Check if user information is available
 if ($user_info) {
-    $image = $user_info['image'];
-    $username = $user_info['name'];
-    $image_path = "image/$image";
+  $image = $user_info['image'];
+  $username = $user_info['name'];
+  $image_path = "image/$image";
 }
 ?>
 
@@ -54,9 +55,9 @@ if ($user_info) {
   <!-- Link Swiper's CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
 
-  <script src="js/home.js"></script>
 
- <!--Navbar-->
+
+  <!--Navbar-->
   <header class="header">
     <div class="logo-container">
       <img class="logo" src="image/logo.png" alt="logo">
@@ -65,165 +66,42 @@ if ($user_info) {
     </div>
     <nav class="navbar">
       <a href="home.php" class="home-active">Home</a>
-      <a href="calendar.php" >Calender</a>
-      <a href="events.php" >Events</a>
+      <a href="calendar.php">Calender</a>
+      <a href="events.php">Events</a>
       <a href="contact.php">Contact</a>
-      <?php if (isset($image) && isset($username)) : ?>
-        <div class="user-profile">
-            <img src="<?php echo $image_path; ?>">
-            <span class="username"><?php echo $username; ?></span>
+      <?php if (isset($image) && isset($username)): ?>
+        <div class="user-profile" onclick="showPopup('user_page.php?id=<?php echo $admin_id; ?>')">
+          <img src="<?php echo $image_path; ?>" alt="User Image">
+          <span class="username">
+            <?php echo $username; ?>
+          </span>
         </div>
-    <?php endif; ?>
-
-
+      <?php endif; ?>
     </nav>
+    <div id="popup-container">
+    <div class="popup-content">
+      <!-- <span class="close-btn" onclick="closePopup()">&times;</span> -->
+      <iframe id="popup-iframe" frameborder="0" allowfullscreen ></iframe>
+
+    </div>
+  </div>
   </header>
+
 
 </head>
 
 <body>
 
+
   <main>
     <section id="section1">
       <img src="image/Event Calender For IIUM's Social Clubs.png" alt="">
     </section>
-    <section class="news">
-      <div class="slider">
-        <div class="list">
-          <div class="item">
-            <div class="slide">
-              <img src="image/legal framework to protect women poster.jpg" alt="">
-            </div>
-          </div>
-          <div class="item">
-            <div class="slide">
-              <img src="image/kfp5076w.png" alt="">
-            </div>
-          </div>
-          <div class="item">
-            <div class="slide">
-              <img src="image/poster.jpeg" alt="">
-            </div>
-          </div>
-          <div class="item">
-            <div class="slide">
-              <img src="image/kfp5076w.png" alt="">
-            </div>
-          </div>
-          <div class="item">
-            <div class="slide">
-              <img src="image/legal framework to protect women poster.jpg" alt="">
-            </div>
-          </div>
-          <div class="item">
-            <div class="slide">
-              <img src="image/legal framework to protect women poster.jpg" alt="">
-            </div>
-          </div>
-        </div>
-        <div class="buttons">
-          <button id="prev">
-            << /button>
-              <button id="next">></button>
-        </div>
-        <ul class="dots">
-          <li class="active"></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-    </section>
-    </div>
-    </div>
-    </section>
+    
 
-    <br><br>
+ 
 
-    <section id="section4">
-      <h3 class="custom-heading">Clubs</h3>
-      <div class="sub-heading">Stay updated with the events hosted by the registered clubs to ensure you don't miss out
-        on any exciting activities.</div>
-      <section class="Clubs_info">
-        <div class="swiper-wrapper">
-          <div class="card ">
-            <div class="card__image">
-              <img src="image/image1.png" alt="card1">
-            </div>
-
-            <div class="card__content"></div>
-            <span class="club__title">Club name</span>
-            <span class="club__name">WAMY IIUM Club</span>
-
-            <p class="card__text">
-              WAMY IIUM Club is an Islamic organization at the International Islamic University Malaysia that promotes
-              Islamic values, community engagement,<span id="dots1">...</span><span id="more1"> and personal development
-                among Muslim students.The club organizes various activities, including
-                educational events, seminars, community service initiatives, and social gatherings, to foster a sense of
-                brotherhood/sisterhood and create a positive impact based on Islamic principles.</span></p>
-            <button onclick="myFunction(1)" id="myBtn1">Read more</button>
-          </div>
-          <div class="card ">
-            <div class="card__image">
-              <img src="image/image2.png" alt="card1">
-            </div>
-
-            <div class="card__content"></div>
-            <span class="club__title">Club name</span>
-            <span class="club__name">Alsalam IIUM Club</span>
-
-            <p class="card__text">Alsalam IIUM Club is an organization at the International Islamic University Malaysia
-              (IIUM) dedicated to promoting peace, unity, and the<span id="dots2">...</span><span id="more2">teachings
-                of Islam among its members and the wider community. The club organizes various activities, including
-                educational events, seminars, community service initiatives, and social gatherings, to foster a sense of
-                brotherhood/sisterhood and create a positive impact based on Islamic principles.</span> </p>
-
-            <button onclick="myFunction(2)" id="myBtn2">Read more</button>
-          </div>
-
-          <div class="card ">
-            <div class="card__image">
-              <img src="image/image3.png" alt="card1">
-            </div>
-
-            <div class="card__content"></div>
-            <span class="club__title">Club name</span>
-            <span class="club__name">​Nibras IIUM Club</span>
-
-            <p class="card__text">Nibras IIUM Club is an organization at the International Islamic University Malaysia
-              (IIUM) that aims to empower and enlighten Muslim students<span id="dots3">...</span><span id="more3">
-                through educational, social, and developmental activities. The club focuses on nurturing leadership
-                skills, fostering Islamic values, and creating a supportive community for personal and spiritual
-                growth.</span></p>
-
-            <button onclick="myFunction(3)" id="myBtn3">Read more</button>
-          </div>
-        </div>
-      </section>
-    </section>
-
-    <section class="quote">
-    </section>
-  </main>
-  <br><br><br><br><br><br>
-  <footer>
-    <div class="footer-content">
-      <h3>2023 Event Calendar For IIUM's Social Club</h3>
-      <p>Website: www.calventer.com<br>
-        Email: calventer@edu.my<br>
-        Follow Us</p>
-      <div class="footer-icons">
-        <i class='bx bxl-facebook'></i>
-        <i class='bx bxl-twitter'></i>
-        <i class='bx bxl-instagram'></i>
-      </div>
-    </div>
-    <div class="footer-bottom">
-      <p>Copyright &copy;2023 calvener. Designed by <span>Ridia and Nouran</span></p>
-    </div>
-  </footer>
-  <script src="script.js"></script>
+  <script src="js/home.js"></script>
 </body>
 
 </html>
--- user_page.php
+++ user_page.php
@@ -6,7 +6,7 @@ session_start();
 
 $admin_id = $_SESSION['admin_id'];
 
-if(!isset($admin_id)){
+if (!isset($admin_id)) {
    header('location:login.php');
 }
 
@@ -14,6 +14,7 @@ if(!isset($admin_id)){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -28,26 +29,32 @@ if(!isset($admin_id)){
    <link rel="stylesheet" href="style/profile.css">
 
 </head>
+
 <body>
 
 
 
-<section class="profile-container">
+   <section class="profile-container">
 
-   <?php
+      <?php
       $select_profile = $conn->prepare("SELECT * FROM `admins` WHERE id = ?");
       $select_profile->execute([$admin_id]);
       $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
-   ?>
+      ?>
 
-   <div class="profile">
-      <img src="uploaded_img/<?= $fetch_profile['image']; ?>" alt="">
-      <h3><?= $fetch_profile['name']; ?></h3>
-      <a href="user_profile_update.php" class="btn">update profile</a>
-      <a href="logout.php" class="delete-btn">logout</a>
-   </div>
+      <div class="profile">
+         <img src="uploaded_img/<?= $fetch_profile['image']; ?>" alt="">
+         <h3>
+            <?= $fetch_profile['name']; ?>
+         </h3>
+         <div class="btn-container">
+            <a href="user_profile_update.php" class="btn">Profile</a>
+            <a href="logout.php" class="delete-btn">Logout</a>
+         </div>
+      </div>
 
-</section>
-
+   </section>
+   <script src="js/home.js"></script>
 </body>
+
 </html>
