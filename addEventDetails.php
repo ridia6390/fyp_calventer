0a3c4ba7cd546e752f87723e85f92c091d89445c
ridia178 <ridia.kashmeri1011@gmail.com>
2023-12-31 22:56:20 +0800

    work

--- addEventDetails.php
+++ addEventDetails.php
@@ -37,55 +37,71 @@ if (isset($_POST["submit"])) {
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
    <!-- Bootstrap -->
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
    <!-- Font Awesome -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+      integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+      crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-   <title>PHP CRUD Application</title>
+   <title> Add Event Details </title>
 </head>
 
 <body>
+
+   <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #078c7c; color: white;">
+      ADD EVENT DETAILS
+   </nav>
+
    <div class="container">
       <div class="container d-flex justify-content-center">
-         <form action="" method="post" style="width:50vw; min-width:300px;">
+         <form action="" method="post" style="width:100vw; min-width:100px; font-weight: 600;">
             <div class="row mb-3">
                <div class="col">
-                  <label class="form-label">First Name:</label>
-                  <input type="text" class="form-control" name="first_name" placeholder="Albert">
+                  <label class="form-label">Event Theme</label>
+                  <input type="text" class="form-control" name="event_theme" placeholder="Cultural Night">
                </div>
 
                <div class="col">
-                  <label class="form-label">Last Name:</label>
-                  <input type="text" class="form-control" name="last_name" placeholder="Einstein">
+                  <label class="form-label">Dress Code</label>
+                  <input type="text" class="form-control" name="dress_code"
+                     placeholder="Traditional Attire">
                </div>
             </div>
 
             <div class="mb-3">
-               <label class="form-label">Email:</label>
-               <input type="email" class="form-control" name="email" placeholder="name@example.com">
+               <label class="form-label">Venue</label>
+               <input type="text" class="form-control" name="venue" placeholder="Main Auditorium">
             </div>
 
-            <div class="form-group mb-3">
-               <label>Gender:</label>
-               &nbsp;
-               <input type="radio" class="form-check-input" name="gender" id="male" value="male">
-               <label for="male" class="form-input-label">Male</label>
-               &nbsp;
-               <input type="radio" class="form-check-input" name="gender" id="female" value="female">
-               <label for="female" class="form-input-label">Female</label>
+            <div class="mb-3">
+               <label class="form-label">Location</label>
+               <input type="text" class="form-control" name="location"
+                  placeholder="https://maps.app.goo.gl/YeL6bzCcGqsoYEhA9">
             </div>
+
+            <div class="mb-3">
+               <label class="form-label">Poster</label>
+               <input type="file" class="form-control" name="image" required class="box"
+                  accept="image/jpg, image/png, image/jpeg">
+            </div>
+
 
             <div>
-               <button type="submit" class="btn btn-success" name="submit">Save</button>
+               <button type="submit" class="btn btn-success" name="submit" style="background-color: #56ab91; border: 1px solid #56ab91">Save</button>
                <a href="events.php" class="btn btn-danger">Cancel</a>
             </div>
+
          </form>
       </div>
    </div>
 
+
    <!-- Bootstrap -->
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+      crossorigin="anonymous"></script>
 
 </body>
 
